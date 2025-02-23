6819367c113e58e477ead1ac99561272348d3680
leejuhoo <leejuhoo@lgcns.com>
2025-02-24 08:27:17 +0900

    change env

--- src/utils/webcam.js
+++ src/utils/webcam.js
@@ -12,7 +12,7 @@ export class Webcam {
         .getUserMedia({
           audio: false,
           video: {
-            facingMode: "environment",
+            // facingMode: "environment",
           },
         })
         .then((stream) => {
